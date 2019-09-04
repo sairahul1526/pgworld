93f07100d0bc7d898476b3f6340e331648a38f4c
sairahul1526 <dravid.rahul1526@gmail.com>
2019-09-05 00:44:45 +0530

    buf fix rollout

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -37,7 +37,7 @@ class CONTACT {
 String mediaURL = "https://test-pgworld.s3.ap-south-1.amazonaws.com/";
 
 class APPVERSION {
-  static const ANDROID = "2.3";
+  static const ANDROID = "2.4";
   static const IOS = "1.1";
 }
 
