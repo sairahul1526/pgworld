79431a6410bf9824f25a50e26e72f19b2b5af75f
sairahul1526 <dravid.rahul1526@gmail.com>
2020-02-10 22:49:42 +0530

    api retry bug

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -12,155 +12,212 @@ import './config.dart';
 // admin
 
 Future<Admins> getAdmins(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.ADMIN, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.ADMIN, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Admins.fromJson(json.decode(response.body));
+    return Admins.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // bill
 
 Future<Bills> getBills(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.BILL, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.BILL, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Bills.fromJson(json.decode(response.body));
+    return Bills.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // dashboard
 
 Future<Dashboards> getDashboards(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.DASHBOARD, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.DASHBOARD, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Dashboards.fromJson(json.decode(response.body));
+    return Dashboards.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // employee
 
 Future<Employees> getEmployees(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.EMPLOYEE, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.EMPLOYEE, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Employees.fromJson(json.decode(response.body));
+    return Employees.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // food
 
 Future<Foods> getFoods(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.FOOD, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.FOOD, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Foods.fromJson(json.decode(response.body));
+    return Foods.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // invoice
 
 Future<Invoices> getInvoices(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.INVOICE, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.INVOICE, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Invoices.fromJson(json.decode(response.body));
+    return Invoices.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // issue
 
 Future<Issues> getIssues(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.ISSUE, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.ISSUE, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Issues.fromJson(json.decode(response.body));
+    return Issues.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // log
 
 Future<Logs> getLogs(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.LOG, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.LOG, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Logs.fromJson(json.decode(response.body));
+    return Logs.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // note
 
 Future<Notes> getNotes(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.NOTE, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.NOTE, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Notes.fromJson(json.decode(response.body));
+    return Notes.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // notice
 
 Future<Notices> getNotices(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.NOTICE, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.NOTICE, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Notices.fromJson(json.decode(response.body));
+    return Notices.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // report
 
 Future<Charts> getReports(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.REPORT, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.REPORT, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-print(response.body);
-  return Charts.fromJson(json.decode(response.body));
+    return Charts.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // room
 
 Future<Rooms> getRooms(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.ROOM, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.ROOM, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Rooms.fromJson(json.decode(response.body));
+    return Rooms.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // user
 
 Future<Users> getUsers(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.USER, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.USER, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Users.fromJson(json.decode(response.body));
+    return Users.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
 
 // hostels
 
 Future<Hostels> getHostels(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.HOSTEL, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.HOSTEL, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Hostels.fromJson(json.decode(response.body));
+    return Hostels.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
-
 
 // status
 
 Future<Admins> getStatus(Map<String, String> query) async {
-  final response = await http
-      .get(Uri.http(API.URL, API.STATUS, query), headers: headers)
-      .timeout(Duration(seconds: timeout));
+  try {
+    final response = await http
+        .get(Uri.http(API.URL, API.STATUS, query), headers: headers)
+        .timeout(Duration(seconds: timeout));
 
-  return Admins.fromJson(json.decode(response.body));
+    return Admins.fromJson(json.decode(response.body));
+  } catch (e) {
+    return null;
+  }
 }
-
 
 // add and update
 Future<bool> add(String endpoint, Map<String, String> body) async {
@@ -178,11 +235,15 @@ Future<bool> add(String endpoint, Map<String, String> body) async {
   request.fields["admin_name"] = adminName;
   body.forEach((k, v) => {request.fields[k] = v});
 
-  var response = await request.send();
-  if (response.statusCode == 200) {
-    return true;
+  try {
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return null;
   }
-  return false;
 }
 
 Future<bool> update(String endpoint, Map<String, String> body,
@@ -195,11 +256,15 @@ Future<bool> update(String endpoint, Map<String, String> body,
   request.fields["admin_name"] = adminName;
   body.forEach((k, v) => {request.fields[k] = v});
 
-  var response = await request.send();
-  if (response.statusCode == 200) {
-    return true;
+  try {
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return null;
   }
-  return false;
 }
 
 Future<bool> delete(String endpoint, Map<String, String> query) async {
@@ -211,11 +276,15 @@ Future<bool> delete(String endpoint, Map<String, String> query) async {
 
   request.headers.addAll(headers);
 
-  var response = await request.send();
-  if (response.statusCode == 200) {
-    return true;
+  try {
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return null;
   }
-  return false;
 }
 
 Future<String> upload(File file) async {
@@ -239,14 +308,19 @@ Future<String> upload(File file) async {
   // add file to multipart
   request.files.add(multipartFile);
 
-  var response = await request.send();
+  try {
+    var response = await request.send();
 
-  if (response.statusCode == 200) {
-    String responseData = await response.stream.transform(utf8.decoder).join();
-    Map data = json.decode(responseData);
-    if (data["data"] != null) {
-      return data["data"][0]["image"];
+    if (response.statusCode == 200) {
+      String responseData =
+          await response.stream.transform(utf8.decoder).join();
+      Map data = json.decode(responseData);
+      if (data["data"] != null) {
+        return data["data"][0]["image"];
+      }
     }
+    return "";
+  } catch (e) {
+    return null;
   }
-  return "";
 }
